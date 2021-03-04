7551a9d821bb3ef3211966cc9e2a12574d4fc594
roqaya299 <elephantsong29@example.com>
2021-03-04 20:11:55 +0200

    sticky navbar

--- js/script.js
+++ js/script.js
@@ -22,13 +22,47 @@
  })
 
 
-
-/*
-window.addEventListener("scroll",function(){
-    var nav = document.querySelector('nav');
-    nav.classList.toggle('sticky-nav', window.scrollY > 0 )
-})*/
-
 function scrollToTop() { 
     window.scrollTo(0,0); 
 } 
+
+
+
+
+/*
+
+window.onscroll = function(){stickyFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("nav-list").style.padding = "0.5rem 1.5rem";
+    document.getElementById("logo").style.width = "200";
+  } else {
+    document.getElementById("navbar").style.padding = "1px 3px";
+    document.getElementById("logo").style.width = "300";
+  }
+}
+*/
+$(window).scroll(function () {
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() >63) {
+      $('.navbar').addClass('sticky');
+      $('#nav-list').addClass('nav-list');
+      $('#logo').addClass('logo');
+
+    }
+    if ($(window).scrollTop() < 64) {
+      $('.navbar').removeClass('sticky');
+    }
+  });
+
