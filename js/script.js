c2049a3dc2b109ddad5c9a0dbed57f22cc0a02aa
roqaya299 <elephantsong29@example.com>
2021-03-04 18:29:35 +0200

    desktop version

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-var loader;
-function loadNow(opacity){
-    if(opacity <= 0) {
-        displayContent();
-    }
-    else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function(){
-            loadNow(opacity - 0.05)
-        }, 100 )
-    }
-}
-function displayContent(){
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block'
-}
-document.addEventListener('DOMContentLoaded' , function(){
-    loader = document.getElementById('loader');
-    loadNow(1);
+
+
+ var loader;
+ function loadNow(opacity){
+     if(opacity <= 0) {
+         displayContent();
+     }
+     else {
+         loader.style.opacity = opacity;
+         window.setTimeout(function(){
+             loadNow(opacity - 1)
+         }, 2000 )
+     }
+ }
+ function displayContent(){
+     loader.style.display = 'none';
+     document.getElementById('content').style.display = 'block'
+ }
+ document.addEventListener('DOMContentLoaded' , function(){
+     loader = document.getElementById('loader');
+     loadNow(1);
+ })
+
+
+window.addEventListener("scroll",function(){
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('sticky-nav', window.scrollY > 0 )
 })
-
-
-// window.addEventListener("scroll",function(){
-//     var nav = document.querySelector('nav');
-//     nav.classList.toggle('sticky-nav', window.scrollY > 0 )
-// })
